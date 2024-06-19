f55350f90e96979f541deef95da1be86727167b0
Camba <fercambafcm@gmail.com>
2024-06-19 12:06:11 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react"
 import ReactDOM from 'react-dom/client'
 
-const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<h1>hola munedo</h1>)
