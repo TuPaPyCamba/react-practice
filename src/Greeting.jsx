d6e50505f20265207a9964effd6daf3075c90a77
Camba <fercambafcm@gmail.com>
2024-06-20 10:51:58 -0600

    update

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,12 +1,19 @@
 export function Greeting({ title, name = "user" }) {
     return (
-    <h1>
-        {title} {name}
-    </h1>
-);
+        <h1>
+            {title} {name}
+        </h1>
+    );
 }
 
-export function UserCard(props) {
-    console.log(props)
-    return <h1>xd</h1>;
+export function UserCard({name, amount, married, address}) {
+    return <div>
+        <h1>{name}</h1>
+        <h1>${amount}</h1>
+        <h1>{married ? 'married' : 'single'}</h1>
+        <ul>
+            <li>city: {address.city}</li>
+            <li>addrees: {address.street}</li>
+        </ul>
+    </div>
 }
