0a106c468c563d08a130ec9e6181d61e6e055e97
Camba <fercambafcm@gmail.com>
2024-06-20 09:32:58 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-import React from "react"
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Greeting() {
-    const married = true
+    const user = true
 
-    if (married) {
-        return <div>
-            <h1> esta casado</h1>
-        </div>
-    } else {
-        return <div>
-            <h1>esta soltero</h1>
-        </div>
-    }
+    return <div>
+        <h1>{user.firstName}</h1>
+    </div>
 }
 
-root.render(<div>
-    <Greeting />
-</div>)
+root.render(<Greeting />);
