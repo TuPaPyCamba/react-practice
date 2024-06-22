81c3548f01406842aa3bcb1b067fa7e472097d18
Camba <fercambafcm@gmail.com>
2024-06-22 11:36:59 -0600

    Update

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -5,6 +5,7 @@ export const Posts = () => {
                 fetch('https://jsonplaceholder.typicode.com/posts')
                 .then(response => response.json())
                 .then(data => console.log(data))
+                .catch(error => console.error(error))
             }}
         >
             traer datos
